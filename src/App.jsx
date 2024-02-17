0a9f89089f120f083bdfeea7a85737ae6ad26976
Victor Raul Gutierrez Sanabria <gutierrezsanabria@gmail.com>
2024-02-17 16:28:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Componente1 from './components/componente1/Componente1'
+
+function App() {
+
+
+  return (
+    <>
+      <Componente1 />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/componente1/Componente1.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Componente1.css'
+
+export default function Componente1() {
+  return (
+    <div className='componente1'>
+      <div className='cuerpo'>
+
+        <img src="./VICTOR.jpg" alt="Foto" />
+
+        <div className='presentacion'>
+          <p> Hi </p>
+          <p> My name is</p>
+          <p className='nombre'> Victor Gutierrez</p>
+          <p>I build things for web</p>
+        </div>
+
+      </div>
+
+
+
+
+    </div>
+  )
+}
